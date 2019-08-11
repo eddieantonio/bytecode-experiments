00e830c4241d52c59dbd8cb2678066b70f61a328
Eddie Antonio Santos <easantos@ualberta.ca>
2019-08-11 11:41:37 -0600

    Rename a few variables.

--- my_interpreter.py
+++ my_interpreter.py
@@ -1,6 +1,6 @@
+from builtins import __dict__ as builtins
 from dataclasses import dataclass
 from opcode import opmap, opname
-from builtins import __dict__ as builtins
 
 
 @dataclass
@@ -17,27 +17,27 @@ def run_python_bytecode(c: MyCodeObject) -> object:
     Returns the code's return value.
     """
 
-    pc = 0  # pc = program counter
-    stack = []
+    program_counter = 0
+    value_stack = []
     local_variables = [None] * c.co_nlocals
 
-    while pc < len(c.co_code):
-        opcode = c.co_code[pc]
-        arg = c.co_code[pc + 1]
+    while program_counter < len(c.co_code):
+        opcode = c.co_code[program_counter]
+        arg = c.co_code[program_counter + 1]
 
         if opcode == opmap['LOAD_CONST']:
-            stack.append(c.co_consts[arg])
+            value_stack.append(c.co_consts[arg])
         elif opcode == opmap['STORE_FAST']:
-            value = stack.pop()
+            value = value_stack.pop()
             local_variables[arg] = value
         elif opcode == opmap['LOAD_FAST']:
             value = local_variables[arg]
-            stack.append(value)
+            value_stack.append(value)
         elif opcode == opmap['BINARY_ADD']:
-            rhs = stack.pop()
-            lhs = stack.pop()
+            rhs = value_stack.pop()
+            lhs = value_stack.pop()
             value = lhs + rhs
-            stack.append(value)
+            value_stack.append(value)
         elif opcode == opmap['LOAD_GLOBAL']:
             name = c.co_names[arg]
             if name in globals():
@@ -46,23 +46,23 @@ def run_python_bytecode(c: MyCodeObject) -> object:
                 value = builtins[name]
             else:
                 raise NameError(f'name {name} is not defined')
-            stack.append(value)
+            value_stack.append(value)
         elif opcode == opmap['CALL_FUNCTION']:
-            argv = []
+            args = []
             for _ in range(arg):
-                argv.append(stack.pop())
-            assert len(stack) >= 1
-            function = stack.pop()
-            value = function(*argv)
-            stack.append(value)
+                args.append(value_stack.pop())
+            assert len(value_stack) >= 1
+            function = value_stack.pop()
+            value = function(*args)
+            value_stack.append(value)
         elif opcode == opmap['POP_TOP']:
-            stack.pop()
+            value_stack.pop()
         elif opcode == opmap['RETURN_VALUE']:
-            return stack.pop()
+            return value_stack.pop()
         else:
             raise NotImplementedError(opname[opcode])
 
-        pc += 2
+        program_counter += 2
 
 run_python_bytecode(MyCodeObject(
     co_code=b'd\x01}\x00d\x02}\x01|\x00d\x03\x17\x00|\x01\x17\x00}\x02t\x00|\x02\x83\x01\x01\x00d\x00S\x00',
